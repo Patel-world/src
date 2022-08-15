38fd7c1c5cc2f8b706e14cb673e2e6226d2e1941
Pankaj Kumar Verma <patel.dp2024@gmail.com>
2022-08-15 11:26:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Spline from '@splinetool/react-spline';
+
+
+
+function App() {
+  return (
+    <Spline scene="https://prod.spline.design/GY5N9MrvNiEkEH9H/scene.splinecode" />
+  );
+}
+
+export default App;
