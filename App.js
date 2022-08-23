9bbdda77dbc4e1d8a43e98d28b483ed83a279aa2
Pankaj Kumar Verma <patel.dp2024@gmail.com>
2022-08-23 10:27:02 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,14 +4,29 @@ import './static/spline.css';
 import Spline from '@splinetool/react-spline';
 import Header from './components/Header';
 import Main from './components/Main';
-
-
+import main from './static/images/main.png'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Update the document title using the browser API
+    var timer = setInterval(function() {
+      
+      if(document.querySelector("#root > canvas").style.display!='none'){
+        document.querySelector("#root > img").style.display='none';
+        clearInterval(timer);
+      } 
+      
+      console.log("I'm here"); //this will still run after clearing
+    }, 200);
+    
+  },[]);
+  
+  
   return (
     <>
     <Header/>
-        
+    <img className='vizx' src={main}></img>
     <Spline scene="https://prod.spline.design/AxDVJGdNbKqLlVbe/scene.splinecode" />
 
     <Main/>
